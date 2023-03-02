036b6b3036d819508aba54c429177de20524a484
plabadie-bzy <126564459+plabadie-bzy@users.noreply.github.com>
2023-03-02 14:21:54 +0000

    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + 'kiwi')
 # Normalise data from fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Put data from fruityvice into a df
