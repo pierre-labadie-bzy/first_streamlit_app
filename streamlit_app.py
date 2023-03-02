c95c7d2f361c2622606b0c3b27be76d6db1d9bc2
plabadie-bzy <126564459+plabadie-bzy@users.noreply.github.com>
2023-03-02 16:41:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 # create function
 def get_fruityvice_data(this_fruit):
-  fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + this fruit)
+  fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + this_fruit)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
